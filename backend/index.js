96f47135a6d5978b2dadee7d72f61e71254b12a4
raja00912cd <mdramiz025@gmail.com>
2023-01-08 22:47:47 +0530

    some packages installed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.static("build"));
 
 app.get("/", (req, res) => {
     res.send({ message: "Hello" });
