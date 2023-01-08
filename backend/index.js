ae2730202b9e3f104620d11a355b809f7a3a67fe
raja00912cd <mdramiz025@gmail.com>
2023-01-08 15:34:31 +0530

    deployed backend with vercel

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,5 @@ const express = require('express')
 const app = express();
 
 app.use("/", (req, res) => {
-    res.json({ message: "Hello" });
-})
-
-const port = 27027;
-
-app.listen(27027, () => {
-    console.log(`starting server on port ${port}`)
+    res.json({ message: "Hello world" });
 })
