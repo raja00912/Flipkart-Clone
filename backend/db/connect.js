4602ddd71995b0f0a41b451a59b15da8d921d517
raja00912cd <mdramiz025@gmail.com>
2023-01-09 09:41:53 +0530

    database deployed with vercel

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -15,4 +15,4 @@ async function connect() {
     })
 }
 
-module.exports = connect;
+module.exports = { connect };
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
-const connect = require('./db/connect');
+const { connect } = require('./db/connect');
 
 const app = express();
 app.use(express.json());
